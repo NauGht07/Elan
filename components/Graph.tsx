0c01352c010b6a5a8a4c88fabdbdab3a6bdc7938
Naught07 <naughtnull@protonmail.com>
2026-06-14 09:17:18 +0530

    removed react flow watermark

--- components/Graph.tsx
+++ components/Graph.tsx
@@ -480,6 +480,7 @@ export default function Graph() {
           d.fx = undefined; d.fy = undefined
         }}
         zoomOnScroll
+        proOptions={{ hideAttribution: true }}
         onInit={(rf) => setViewport(rf.getViewport())}
         onMove={(_, vp) => setViewport(vp)}
         onMoveStart={() => { userOverrideRef.current = true }}
